5472ea89046323349971d19a6388cc42f7e219d0
Wladimir W. Shatovsky <shutofsky@gmail.com>
2019-10-22 11:21:08 +0000

    Допиленная версия.

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-##############################################################################################
-# Управляющая программа для "люстры" нового поколения.
-# Пока обработка только RGB - канала устройства, на основе будет допилена прошивка для 
-# полного функционала.
-#
-# Управляющий топик - RGB и RGB<mac-address>. Ответы в топике RGBASK. Ответ на пинг не 
-# реализован пока.
-# 
-# Формат фходной команды - JSON. Для RGB выглядит так:
-# 
-# {"RGB":"FF0000/3000/1000/00FF00/3000/2000/0000FF/3000/0/C"}
-#         цвет1  вр.1 вр.2 цвет2  вр.1 вр.2 цвет3  вр.1 вр2 последовательность
-# Количество цветов в одной последвоательности произвольно.
-#
-# Время 1 - время, сколько цвет находится "в статике" в миллисекундах. 0 не допускается. 
-# Для этого показателя допускается указание диапазона времени в милолсекундах через "-",
-# например 100-1000 - это означает, что каждый раз время свечения выбираестя случайно
-# в заданном диапазоне.
-#
-# Время 2 - время "перехода" из одного цвета к следующему, меньще 1000 мс указывать не 
-# рекомендуется. Допустимо указывать 0 - мгновенная смена цвета.
-#
-# Цвета обязательно указываются 6-значными 16-ричными значениями, скоращать нельзя.
-#
-##############################################################################################
 import machine
 import time
 import math
 import ujson
 import network
+
 import urandom
 
 quantNum = 20
@@ -50,16 +26,23 @@ quantFlag = 0
 rD = 0
 bD = 0
 gD = 0
-
 lastRGB = 0
 
-redPin = machine.Pin(15, machine.Pin.OUT)
-greenPin = machine.Pin(12, machine.Pin.OUT)
-bluePin = machine.Pin(13, machine.Pin.OUT)
+lenSTR = 0
+modeSTR = ''
+onOffSTR = []
+timeCurrentSTR = 0
+countCurrentSTR = 0
+timeSliceSTR = 0
+timeSTR = []
 
-redPWM = machine.PWM(redPin, freq=1000)
-greenPWM = machine.PWM(greenPin, freq=1000)
-bluePWM = machine.PWM(bluePin, freq=1000)
+lenLGT = 0
+modeLGT = ''
+onOffLGT = []
+timeCurrentLGT = 0
+countCurrentLGT = 0
+timeSliceLGT = 0
+timeLGT = []
 
 def randint(min, max):
     span = max - min + 1
@@ -69,54 +52,122 @@ def randint(min, max):
     return val
 
 def parse_command(newCom):
-  global modeRGB,lenRGB,colorRGB,timeStaticRGB,timeChangeRGB,timeCurrentRGB
-  global timeSliceRGB,curCom,rD,bD,gD
-  if newCom['RGB'] != curCom['RGB']:
-    # Меняем команду
-    curCom['RGB'] = newCom['RGB']
-    fields = curCom['RGB'].split('/')
-    modeRGB = fields[len(fields)-1]
-    lenRGB = int(len(fields)/3)
-    del colorRGB[:]
-    del timeStaticRGB[:]
-    del timeChangeRGB[:]
-    i = 0
-    while i < lenRGB:
-      colorRGB.append(fields[i*3])
-      timeStaticRGB.append(fields[i*3+1])
-      timeChangeRGB.append(int(fields[i*3+2]))
-      i = i + 1
-    rD = int(colorRGB[0][0:2],16)*4
-    gD = int(colorRGB[0][2:4],16)*4
-    bD = int(colorRGB[0][4:6],16)*4
-    redPWM.duty(rD)
-    greenPWM.duty(gD)
-    bluePWM.duty(bD)
-    timeRandomRGB = str(timeStaticRGB[0]).split('-')
-    if len(timeRandomRGB) != 2:
-      timeSRGB = int(timeRandomRGB[0])
-    else:
-      timeSRGB = int(randint(int(timeRandomRGB[0]), int(timeRandomRGB[1])))
-    timeSliceRGB = timeSRGB
-    countCurrentRGB = 0
-    quantCount = 0
-    quantFlag = 0
-    timeCurrentRGB = time.ticks_ms()
+  global modeRGB,lenRGB,colorRGB,timeStaticRGB,timeChangeRGB,timeCurrentRGB,timeSliceRGB,curCom,rD,bD,gD
+  global lenSTR, modeSTR,onOffSTR,timeCurrentSTR,countCurrentSTR,timeSliceSTR
+  global lenLGT, modeLGT,onOffLGT,timeCurrentLGT,countCurrentLGT,timeSliceLGT
+  if 'RGB' in newCom:
+    if newCom['RGB'] != curCom['RGB']:
+      fields = newCom['RGB'].split('/')
+      if len(fields) < 3:
+        if fields[0] == u'RESET':
+          machine.reset()
+          return
+        else:
+          return
+      if (len(fields)-1)%3 != 0:
+        return
+      curCom['RGB'] = newCom['RGB']
+      modeRGB = fields[len(fields)-1]
+      lenRGB = int(len(fields)/3)
+      del colorRGB[:]
+      del timeStaticRGB[:]
+      del timeChangeRGB[:]
+      i = 0
+      while i < lenRGB:
+        colorRGB.append(fields[i*3])
+        timeStaticRGB.append(fields[i*3+1])
+        timeChangeRGB.append(int(fields[i*3+2]))
+        i = i + 1
+      rD = int(colorRGB[0][0:2],16)*4
+      gD = int(colorRGB[0][2:4],16)*4
+      bD = int(colorRGB[0][4:6],16)*4
+      redPWM.duty(rD)
+      greenPWM.duty(gD)
+      bluePWM.duty(bD)
+      timeRandomRGB = str(timeStaticRGB[0]).split('-')
+      if len(timeRandomRGB) != 2:
+        timeSRGB = int(timeRandomRGB[0])
+      else:
+        timeSRGB = int(randint(int(timeRandomRGB[0]), int(timeRandomRGB[1])))
+      timeSliceRGB = timeSRGB
+      countCurrentRGB = 0
+      quantCount = 0
+      quantFlag = 0
+      timeCurrentRGB = time.ticks_ms()
+  if 'STR' in newCom:
+    if newCom['STR'] != curCom['STR']:
+      fields = newCom['STR'].split('/')
+      if len(fields) < 2:
+        if fields[0] == u'RESET':
+          machine.reset()
+          return
+        else:
+          return
+      if (len(fields)-1)%2 != 0:
+        return
+      curCom['STR'] = newCom['STR']
+      modeSTR = fields[len(fields)-1]
+      lenSTR = int(len(fields)/2)
+      del onOffSTR[:]
+      del timeSTR[:]
+      i = 0
+      while i < lenSTR:
+        onOffSTR.append(fields[i*2])
+        timeSTR.append(fields[i*2+1])
+        i = i + 1
+      pinSTR.value(onOffSTR[0])
+      timeRandomSTR = str(timeSTR[0]).split('-')
+      if len(timeRandomSTR) != 2:
+        timeS = int(timeRandomSTR[0])
+      else:
+        timeS = int(randint(int(timeRandomSTR[0]), int(timeRandomSTR[1])))
+      timeSliceSTR = timeS
+      countCurrentSTR = 0
+      timeCurrentSTR = time.ticks_ms()
+  if 'LGT' in newCom:
+    if newCom['LGT'] != curCom['LGT']:
+      fields = newCom['LGT'].split('/')
+      if len(fields) < 2:
+        if fields[0] == u'RESET':
+          machine.reset()
+          return
+        else:
+          return
+      if (len(fields)-1)%2 != 0:
+        return
+      curCom['LGT'] = newCom['LGT']
+      modeLGT = fields[len(fields)-1]
+      lenLGT = int(len(fields)/2)
+      del onOffLGT[:]
+      del timeLGT[:]
+      i = 0
+      while i < lenLGT:
+        onOffLGT.append(fields[i*2])
+        timeLGT.append(fields[i*2+1])
+        i = i + 1
+      pinLGT.value(onOffLGT[0])
+      timeRandomLGT = str(timeLGT[0]).split('-')
+      if len(timeRandomLGT) != 2:
+        timeSL = int(timeRandomLGT[0])
+      else:
+        timeSL = int(randint(int(timeRandomLGT[0]), int(timeRandomLGT[1])))
+      timeSliceLGT = timeSL
+      countCurrentLGT = 0
+      timeCurrentLGT = time.ticks_ms()
 
 def sub_cb(topic, msg):
-  print((topic, msg))
   global newCom, myMAC, topic_sub, topic_sub_id
   if topic == topic_sub or topic == topic_sub_id:
     try:
       newCom = ujson.loads(msg)
     except:
-      print ('Wrong command')
       time.sleep_ms(200)
       return
     parse_command(newCom)
     
 def connect_and_subscribe():
   global client_id, mqtt_server, topic_sub, myMAC
+  global redPWM, greenPWM, bluePWM
   client = MQTTClient(client_id, mqtt_server)
   client.set_callback(sub_cb)
   client.connect()
@@ -125,26 +176,20 @@ def connect_and_subscribe():
   print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
   print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub_id))
   greenPin.value(0)  
+  ## Не забыть раскомментировать три следующих строки перед реальным использвоанием!!!
+  #redPWM = machine.PWM(redPin, freq=1000)
+  #greenPWM = machine.PWM(greenPin, freq=1000)
+  #bluePWM = machine.PWM(bluePin, freq=1000)
+  client.publish(topic_pub+'/'+myMAC, 'CUP/{"lts":"'+str(time.ticks_ms())+'"}')
   return client
 
 def restart_and_reconnect():
   print('Failed to connect to MQTT broker. Reconnecting...')
-  greenPin.value(1)
-  time.sleep_ms(250)
-  greenPin.value(0)
-  time.sleep_ms(250)
-  greenPin.value(1)
-  time.sleep_ms(250)
-  greenPin.value(0)
-  time.sleep_ms(250)
-  greenPin.value(1)
-  time.sleep_ms(250)
-  greenPin.value(0)
-  time.sleep_ms(250)
-  greenPin.value(1)
-  time.sleep_ms(250)
-  greenPin.value(0)
-  time.sleep_ms(250)
+  for x in range(10):
+    greenPin.value(1)
+    time.sleep_ms(250)
+    greenPin.value(0)
+    time.sleep_ms(250)
   machine.reset()
 
 try:
@@ -166,10 +211,9 @@ while True:
           countCurrentRGB += 1
           if countCurrentRGB >= lenRGB:
             lenRGB = 0
-            curCom['RGB'] = ''
             continue
         if timeChangeRGB[numBefore] > 0 and quantFlag == 0 :
-          # Плавная смена цвета. 
+          # Плавное изменение цвета
           timeSliceRGB = int(timeChangeRGB[numBefore]/quantNum)
           rDDelta = int((int(colorRGB[countCurrentRGB][0:2],16)*4 - int(colorRGB[numBefore][0:2],16)*4)/quantNum)
           gDDelta = int((int(colorRGB[countCurrentRGB][2:4],16)*4 - int(colorRGB[numBefore][2:4],16)*4)/quantNum)
@@ -202,6 +246,44 @@ while True:
         greenPWM.duty(gD)
         bluePWM.duty(bD)
         timeCurrentRGB = time.ticks_ms()
+    if lenSTR > 0:
+      if (time.ticks_ms() - timeCurrentSTR) >= timeSliceSTR:
+        # Смена такта
+        if modeSTR == 'C':
+          countCurrentSTR = (countCurrentSTR + 1) % lenSTR
+        elif modeSTR == 'S':
+          countCurrentSTR += 1
+          if countCurrentSTR >= lenSTR:
+            lenSTR = 0
+            continue
+        timeRandomSTR = str(timeSTR[countCurrentSTR]).split('-')
+        if len(timeRandomSTR) != 2:
+          timeS = int(timeRandomSTR[0])
+        else:
+          timeS = randint(int(timeRandomSTR[0]), int(timeRandomSTR[1]))
+          timeSliceSTR = timeS
+        pinSTR.value(int(onOffSTR[countCurrentSTR]))
+        timeCurrentSTR = time.ticks_ms()
+    if lenLGT > 0:
+      if (time.ticks_ms() - timeCurrentLGT) >= timeSliceLGT:
+        # Смена такта
+        if modeLGT == 'C':
+          countCurrentLGT = (countCurrentLGT + 1) % lenLGT
+        elif modeLGT == 'S':
+          countCurrentLGT += 1
+          if countCurrentLGT >= lenLGT:
+            lenLGT = 0
+            continue
+        timeRandomLGT = str(timeLGT[countCurrentLGT]).split('-')
+        if len(timeRandomLGT) != 2:
+          timeSL = int(timeRandomLGT[0])
+        else:
+          timeSL = randint(int(timeRandomLGT[0]), int(timeRandomLGT[1]))
+          timeSliceLGT = timeSL
+        pinLGT.value(int(onOffLGT[countCurrentLGT]))
+        timeCurrentLGT = time.ticks_ms()
+      
   except OSError as e:
     restart_and_reconnect()
+
 
