fceb442f97c4f446a2ce09481c86b33a2dc22098
Wladimir W. Shatovsky <shutofsky@gmail.com>
2019-12-09 14:24:50 +0000

    Replace config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import machine
 from network import WLAN
 from ubinascii import hexlify
 
-ENV = 'dev'  # 褍褋褌邪薪邪胁谢懈胁邪械屑 锌械褉械屑械薪薪褍褞 芯泻褉褍卸械薪懈褟
+ENV = 'dev'  
 
 cfg = {
     'broker_ip': '192.168.137.1',
@@ -11,16 +11,17 @@ cfg = {
     'last_message': 0,
     'message_interval': 5,
     'counter': 0,
-    'wlan_ssid': 'P2797-24',  # 懈屑械薪邪 写谢懈薪薪褘械, 蟹邪褌芯 褌芯褔薪芯 胁褋械 锌芯薪褟褌薪芯
-    'wlan_password': 'z0BcfpHu'
+    'wlan_ssid': 'P2797-24',  
+    'wlan_password': 'z0BcfpHu',
+    'quant_num': 50
 }
 
 pins = {
     'red': machine.Pin(15, machine.Pin.OUT),
     'green': machine.Pin(12, machine.Pin.OUT),
     'blue': machine.Pin(13, machine.Pin.OUT),
-    'STR': machine.Pin(15, machine.Pin.OUT),  # 褋懈褉械薪邪 薪邪 泻褉邪褋薪芯屑 锌懈薪械
-    'LGT': machine.Pin(12, machine.Pin.OUT)  # 斜械谢褘泄 褋胁械褌 薪邪 蟹械谢械薪芯屑 锌懈薪械
+    'STR': machine.Pin(15, machine.Pin.OUT),  
+    'LGT': machine.Pin(12, machine.Pin.OUT)  
 }
 
 topics = {
@@ -29,4 +30,6 @@ topics = {
     'pub_id': b'RGBASK/' + cfg['mac'],
     'sub_id': b'RGB/' + cfg['mac']
 }
+
+
 
